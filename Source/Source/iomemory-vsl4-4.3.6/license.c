4d0bd145cd484d2a576151ee16306b7a467540d7
Michael Hansen <mha@asom-net.dk>
2020-06-23 12:41:40 +0200

    Support for Kernel 4.19

---
+++ Source/Source/iomemory-vsl4-4.3.6/license.c
@@ -0,0 +1,7 @@
+/* Machine generated file - edits will be overwritten. */
+
+#include <linux/module.h>
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Western Digital Technologies, Inc.");
+MODULE_DESCRIPTION("Fusion ioMemory-VSL (Virtual Storage Layer)");
+MODULE_INFO(srcversion, "4.3.6.1173 bryce@19486b4134b2");
